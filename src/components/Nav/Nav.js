7fed3d5d2021e41ba0a7bf911a73b1c50b3287b5
Kevin Cheung <kevinchikitcheung@gmail.com>
2018-09-19 20:31:30 -0500

    added css to burger and links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,11 +3,23 @@ import React, { Component } from 'react';
 class Nav extends Component{
     constructor(){
         super();
+        this.state = {
+            menuSlide: false
+        }
+        this.openMenu = this.openMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
         this.homeScroll = this.homeScroll.bind(this);
         this.aboutScroll = this.aboutScroll.bind(this);
         this.skillScroll = this.skillScroll.bind(this);
         this.projScroll = this.projScroll.bind(this);
         this.expScroll = this.expScroll.bind(this);
+    }
+    openMenu = (e) => {
+        e.preventDefault();
+        this.setState({ menuSlide: !this.state.menuSlide }, () => { document.addEventListener('click', this.closeMenu)});
+    }
+    closeMenu = () => {
+        this.setState({ menuSlide: false }, () =>{ document.removeEventListener('click', this.closeMenu) });
     }
     homeScroll = () => {
         window.scrollTo({ top: 0, behavior: "smooth"});
@@ -23,18 +35,23 @@ class Nav extends Component{
     }
     expScroll = () => {
         window.scrollTo({ top: 2000, behavior: "smooth"});
-    }
-    
+    } 
     render(){
         return(
             <div className='navbar'>
-                <ul>
-                    <li className='logo' onClick={this.homeScroll}>Logo</li>
-                    <li className='links' onClick={this.aboutScroll}>About Me</li>
-                    <li className='links' onClick={this.skillScroll}>Skills</li>
-                    <li className='links' onClick={this.projScroll}>Projects</li>
-                    <li className='links' onClick={this.expScroll}>Experience</li>
-                    <li className='links' onClick={this.homeScroll}>Home</li>
+                <p className='logo' onClick={this.homeScroll}>Logo</p>
+                <div className='burger'>
+                {/* need to put onClick in this div */}
+                    <span/>
+                    <span/>
+                    <span/>
+                </div>
+                <ul className='nav-links'>
+                    <li className='link' onClick={this.aboutScroll}>About Me</li>
+                    <li className='link' onClick={this.skillScroll}>Skills</li>
+                    <li className='link' onClick={this.projScroll}>Projects</li>
+                    <li className='link' onClick={this.expScroll}>Experience</li>
+                    <li className='link' onClick={this.homeScroll}>Home</li>
                 </ul>
             </div>
         );
